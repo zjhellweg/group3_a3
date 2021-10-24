68d0ab5ca163ce16665cd98b2427779d8d27ea68
cquigley2020 <61098258+cquigley2020@users.noreply.github.com>
2021-10-23 21:28:56 -0500

    Create Quigley.dart

---
+++ Quigley.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:group3a3/main.dart';
+
+class Quigley extends StatefulWidget {
+  const Quigley({Key? key}) : super(key: key);
+
+  @override
+  _Quigley createState() => _Quigley();
+}
+
+class _Quigley extends State<Quigley> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text("Charissa's Page"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Center(
+                child: Container(
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: Text("My birthday is exactly halfway to Christmas.",
+                  style: TextStyle(fontSize: 30), textAlign: TextAlign.center),
+            )),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => MyApp()));
+              },
+              child: Text("GOTO Main"),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
